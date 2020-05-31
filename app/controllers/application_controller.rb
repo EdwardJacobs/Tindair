27b1c8f0ce08f16fade80948cf057282c2c35558
Edward Jacobs <eddielawrence23@gmail.com>
2020-05-30 21:03:15 -0500

    Style slideshow and add image upload with active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_devise, if: :devise_controller?
 
+  private
+
+  def configure_devise
+    devise_parameter_sanitizer.permit(:user_update, keys: [ :first_name, :last_name, :username, images: [] ])
+  end
 
 end
