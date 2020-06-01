753f815d5984f694d6a854d32780bf2897bd27f1
Edward Jacobs <eddielawrence23@gmail.com>
2020-06-01 14:22:25 -0500

    Hard code image for temporary work around

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create([
-  {first_name: "Krista", last_name: "Jones", username: "krista", email: "krista@example.com", password: "testtest", photo: 'https://www.santpaubarcelona.org/sites/default/files/styles/news_teaser/public/flickr/12835239003_0fda5fb8ab_1393612375.jpg?itok=U2gP9jjg'},
+  {first_name: "Krista", last_name: "Jones", username: "krista", email: "krista@example.com", password: "testtest"},
   {first_name: "Mariana", last_name: "Jones", username: "mariana", email: "mariana@example.com", password: "testtest"},
   {first_name: "Gisele", last_name: "Jones", username: "gisele", email: "gisele@example.com", password: "testtest"},
   {first_name: "Danica", last_name: "Jones", username: "danica", email: "danica@example.com", password: "testtest"},
