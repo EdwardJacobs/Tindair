f09292c1ab033c042c4157557935cef64409dd1e
Edward Jacobs <eddielawrence23@gmail.com>
2020-06-01 17:33:56 -0500

    Add approve logic for likes

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,16 +1,31 @@
 class BrowseController < ApplicationController
 
   def browse
-    @users = User.where.not(id: current_user)
+    liked_user_ids = Like.where(user_id: current_user.id).map(&:liked_user_id)
+    liked_user_ids << current_user.id
+    @users = User.where.not(id: liked_user_ids)
   end
 
   def approve
+    user_id = params[:id]
+    # user swipes right
     logger.debug "User id for matching is #{params[:id]}"
 
+    # create like for user
+    new_like = Like.new(liked_user_id: user_id)
+    new_like.user_id = current_user.id
+
+    if new_like.save
+      # check if user already likes us back
+      existing_like = Like.where(user_id: user_id, liked_user_id: current_user.id).count
+      @they_like_us = existing_like > 0
+    else
+      # issue saving like - return a warning message
+    end
   end
 
   def decline
-
+    # user swipes left
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_231227) do
+ActiveRecord::Schema.define(version: 2020_06_01_203248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2020_05_31_231227) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "liked_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
