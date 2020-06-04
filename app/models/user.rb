dacdb3c632b2da925b1222a19f5bf2531cf22094
Edward Jacobs <eddielawrence23@gmail.com>
2020-06-04 18:24:24 -0500

    Render images from remote url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  mount_uploader :photo, PhotoUploader
+  # mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :photo
+  # has_one_attached :photo
 
   def matches
     liked_user_ids = Like.where(user_id: self.id).map(&:liked_user_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 
-User.create([
-  {first_name: "Kristin", last_name: "Jones", username: "kristin", email: "kristin@example.com", password: "testtest"},
-  {first_name: "Jane", last_name: "Jones", username: "jane", email: "jane@example.com", password: "testtest"},
-  {first_name: "Mel", last_name: "Jones", username: "mel", email: "mel@example.com", password: "testtest"},
-  {first_name: "Flo", last_name: "Jones", username: "flo", email: "flo@example.com", password: "testtest"},
-  {first_name: "Angela", last_name: "Jones", username: "angela", email: "angela@example.com", password: "testtest"}
+users = User.create!([
+  {
+    first_name: "Kristin",
+    last_name: "Jones",
+    username: "kristin",
+    email: "kristin@example.com",
+    password: "testtest",
+    photo: "https://images.pexels.com/photos/862848/pexels-photo-862848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    first_name: "Jane",
+    last_name: "Jones",
+    username: "jane",
+    email: "jane@example.com",
+    password: "testtest",
+    photo: "https://images.pexels.com/photos/1090393/pexels-photo-1090393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    first_name: "Mel",
+    last_name: "Jones",
+    username: "mel",
+    email: "mel@example.com",
+    password: "testtest",
+    photo: "https://images.pexels.com/photos/569163/pexels-photo-569163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    first_name: "Flo",
+    last_name: "Jones",
+    username: "flo",
+    email: "flo@example.com",
+    password: "testtest",
+    photo: "https://images.pexels.com/photos/40815/youth-active-jump-happy-40815.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    first_name: "Angela",
+    last_name: "Jones",
+    username: "angela",
+    email: "angela@example.com",
+    password: "testtest",
+    photo: "https://images.pexels.com/photos/247204/pexels-photo-247204.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  }
 ])
+
+puts 'Users:'
+users.each do |user|
+  puts "#{user.username}"
+end
+
+
